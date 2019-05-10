3376a3f74f287b5e370de3d49a20266dfe08eaf4
Tobias <tobz0r7@gmail.com>
2019-05-10 19:10:04 +0200

    Update tetris.js

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -7,7 +7,7 @@ context.scale(20,20);
  	let rowcount=0;
  	outer: for(let y=arena.length-1; y > 0; y--){
  		for(let x=0; x < arena[y].length;x++){
- 			if(arema[y][x]===0){
+ 			if(arena[y][x]===0){
  				continue outer;
  			}
  		}
